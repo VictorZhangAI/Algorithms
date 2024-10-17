8b7318ba251b5238052c184f8951ab9fd9f058d7
VZstless <2319725508@qq.com>
2024-10-17 16:49:32 +0800

    finished algorithm analysis

---
+++ analysis/3sum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define DATA_SIZE 4096
+using namespace std;
+
+vector<int> numbers;
+
+int count()
+{
+	int N = numbers.size();
+	int count = 0;
+	for(int i = 0; i < N; i++)
+		for(int j = i + 1; j < N; j++)
+			for(int k = i + 2; k < N; k++)
+				if(numbers[i] + numbers[j] + numbers[k] == 0)
+					count++;
+	return count;
+}
+
+int main()
+{
+	srand((unsigned)time(NULL));
+	for(int i = 0; i < DATA_SIZE; i++){
+		numbers.push_back(rand() % 100 - 100);
+	}
+	clock_t start = clock();
+	int pairs = count();
+	clock_t end = clock();
+	printf("There are %d pairs of 3-sums.\n", pairs);
+	printf("Time used: %lf\n", (double)(end - start) / CLOCKS_PER_SEC);
+	return 0;
+}
